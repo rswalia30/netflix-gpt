977d2ed3786eaced8f56da34d0fbba26c7c54329
Rohit <2020010006264@gndu.ac.in>
2024-08-14 15:48:37 +0530

    cra & tailwind setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="m-2 p-2 text-red-700 text-3xl font-bold">
+        Hello Netflix-GPT
+      </h1>
+    </>
+  );
+};
+
+export default App;
